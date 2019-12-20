29330f742a5a29fd27a60821a187eda486c53d7e
Alex Mendes <mendes.develop@gmail.com>
2019-12-20 22:00:52 +0000

    Function completed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-String.prototype.toJadenCase = function () {
+String.prototype.capitalize = function () {
   let stringArray = this.split(/(\s+)/)
   let newArray = stringArray.map(word => word[0].toUpperCase() + word.slice(1))
   return newArray.join("")
